684657f6b317d961154bb6c9ac34d2bac89df471
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-09-02 09:25:29 -0500

    Modified the assign role to 'user'

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ])->assignRole('Director');
+        ])->assignRole('Usuario');
     }
 }
