ed76ed3823d709d857b60803f2d569411f23bfb6
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-11-25 08:04:44 -0600

    Modified home view with ticktes section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,14 +40,6 @@
                             </div>
                         </div>
                     </div>
-        <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Tickets</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Seccion de Tickets</h6>
-                    <p class="card-text">En esta seccion se podran ver todas las opciones sobre los tickets.</p>
-                <a href="{{route('new_ticket')}}" class="card-link">Generar ticket</a>
-                <a href="{{route('list_tickets')}}" class="card-link">Lista de Tickets</a>
                 </div>
             </div>
         </div>
@@ -58,19 +50,20 @@
                         <div class="media d-flex">
                             <div class="media-body text-left">
                                 <h3 class="danger">Archivos</h3>
-                                <span><a href="{{route('document_index')}}" class="btn btn-sm btn-link">Acceder <i
-                                            class="fas fa-external-link-square-alt"></i></a></span>
+                                <span><a href="{{route('document_index')}}" class="btn btn-sm btn-link">Acceder
+                                        <i class="fas fa-external-link-square-alt"></i></a></span>
                             </div>
                             <div class="align-self-center">
-                                <p class="small text-muted">Módulo dedicado al manejo de la carga de archivos.</p>
+                                <p class="small text-muted">Módulo dedicado al manejo de la carga de
+                                    archivos.</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-</div>
-<div class="row my-3">
+    </div>
+    <div class="row my-3">
         <div class="col-xl-4 col-sm-6 col-12">
             <div class="card shadow-sm">
                 <div class="card-content">
@@ -78,8 +71,8 @@
                         <div class="media d-flex">
                             <div class="media-body text-left">
                                 <h3 class="danger">Usuarios</h3>
-                                <span><a href="{{route('user_index')}}" class="btn btn-sm btn-link">Acceder <i
-                                            class="fas fa-external-link-square-alt"></i></a></span>
+                                <span><a href="{{route('user_index')}}" class="btn btn-sm btn-link">Acceder
+                                        <i class="fas fa-external-link-square-alt"></i></a></span>
                             </div>
                             <div class="align-self-center">
                                 <p class="small text-muted">Módulo dedicado al manejo de los usuarios.</p>
@@ -100,7 +93,29 @@
                                             class="fas fa-external-link-square-alt"></i></a></span>
                             </div>
                             <div class="align-self-center">
-                                <p class="small text-muted">Módulo dedicado para la sección de preguntas frecuentes.</p>
+                                <p class="small text-muted">Módulo dedicado para la sección de preguntas
+                                    frecuentes.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 col-12">
+            <div class="card shadow-sm">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="danger">Tickets</h3>
+                                <span><a href="{{route('new_ticket')}}" class="btn btn-sm btn-link">Generar ticket<i
+                                            class="fas fa-external-link-square-alt"></i></a></span>
+                                <span><a href="{{route('list_tickets')}}" class="btn btn-sm btn-link">Lista de tickets
+                                        <i class="fas fa-external-link-square-alt"></i></a></span>
+                            </div>
+                            <div class="align-self-center">
+                                <p class="small text-muted">Módulo dedicado para la sección de ticktes creados por
+                                    usuarios.</p>
                             </div>
                         </div>
                     </div>
@@ -110,8 +125,4 @@
     </div>
 </div>
 
-<<<<<<< HEAD
 @endsection
-=======
-@endsection
->>>>>>> a3d3d2aed49db91a928d32eced8912eeee2bf868
