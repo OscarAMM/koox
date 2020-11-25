86aa6aa7685dd564fd436f8f53d6d9d6d52f35c0
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-11-25 09:26:14 -0600

    Format

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Ticket;
 use App\User;
+use Illuminate\Support\Facades\Auth;
+
 class TicketsController extends Controller
 {
     public function __construct()
@@ -14,18 +14,18 @@ class TicketsController extends Controller
     }
     public function view_create()
     {
-     $ticket = Ticket::all();
-     $user = Auth::user();
+        $ticket = Ticket::all();
+        $user = Auth::user();
         return view('tickets.create_tickets', compact('ticket'));
     }
-     public function create()
+    public function create()
     {
         $users = User::where('id', '<>', Auth::user()->id)->get();
         return view('forum.create', compact('users'));
     }
 
-   // public function view_list()
+    // public function view_list()
     //{
-      //  return view('tickets.show_tickets');
+    //  return view('tickets.show_tickets');
     //}
 }
