d58bd3098f68339491e572e1eb1e58435927168a
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-12-07 15:04:14 -0600

    Icons changed to black color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,35 +10,35 @@
     <div class="row my-3">
         <!-- Team item -->
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-comments fa-3x" style="color: #F18B39"></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-comments fa-3x"></i>
                 <h5 class="mb-0">Foro</h5><span class="small text-uppercase text-muted">Módulo dedicado para supervisar
                     los foros creados.</span>
                     <p><a href="{{route('forum_index')}}" class="btn btn-sm btn-primary">Acceder </a></p>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4" ><i class="fas fa-clone fa-3x" style="color: #F18B39"></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4" ><i class="fas fa-clone fa-3x" ></i>
                 <h5 class="mb-0">Principal</h5><span class="small text-uppercase text-muted">
                     Módulo dedicado al contenido de la página principal.</span>
                     <p><a href="{{route('content_index')}}" class="btn btn-sm btn-primary">Acceder </a></p>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-file-upload fa-3x" style="color: #F18B39" ></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-file-upload fa-3x" ></i>
                 <h5 class="mb-0">Archivos</h5><span class="small text-uppercase text-muted">
                     Módulo dedicado al manejo de la carga de archivos.</span>
                     <p><a href="{{route('document_index')}}" class="btn btn-sm btn-primary">Acceder </a></p>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-users fa-3x" style="color: #F18B39"></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-users fa-3x"></i>
                 <h5 class="mb-0">Usuarios</h5><span class="small text-uppercase text-muted">Módulo dedicado al manejo de
                     los usuarios</span>
                     <p><a href="{{route('user_index')}}" class="btn btn-sm btn-primary">Acceder </a></p>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-question-circle fa-3x" style="color: #F18B39"></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-question-circle fa-3x"></i>
                 <h5 class="mb-0">
                     FAQ</h5><span class="small text-uppercase text-muted">Módulo dedicado para la sección de preguntas
                     frecuentes.</span>
@@ -47,14 +47,14 @@
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-file fa-3x" style="color: #F18B39"></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-file fa-3x" ></i>
                 <h5 class="mb-0">Tickets</h5><span class="small text-uppercase text-muted">Módulo dedicado para la
                     sección de ticktes creados por usuarios</span>
                     <p><a href="{{route('list_tickets')}}" class="btn btn-sm btn-primary">Acceder</a></p>
             </div>
         </div>
         <div class="col-xl-3 col-sm-6 mb-5 text-center">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-users fa-3x" style="color: #F18B39"></i>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><i class="fas fa-user-tie fa-3x"></i>
                 <h5 class="mb-0">Perfiles</h5><span class="small text-uppercase text-muted">Módulo dedicado para la
                     sección de perfiles y archivos relacionados </span>
                     <p><a href="{{route('profiles_index')}}" class="btn btn-sm btn-primary">Acceder</a></p>
