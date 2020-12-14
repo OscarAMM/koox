275868b2b465fd965fa5f363088996e744687832
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-12-14 13:31:02 -0600

    New file input added

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -7,10 +7,9 @@
             <div class="card box shadow">
                 <div class="card-body">
                     <div class="py-2 text-center">
-                    <i class="fas fa-question-circle fa-2x"></i>
-                        <h3>FAQ</h3>
+                        <h3><i class="fas fa-question-circle"></i> FAQ</h3>
                     </div>
-                    <form action="{{route('questions_store')}}" method="post">
+                    <form action="{{route('questions_store')}}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="row">
                             <div class="col-lg-12">
@@ -23,8 +22,8 @@
                         <div class="row">
                             <div class="col-lg-12">
                                 <label for="answer">Respuesta</label>
-                                <textarea name="answer" id="answer" cols="20" rows="5" class="form-control" autocomplete="off">
-                                    </textarea>
+                                <textarea name="answer" id="answer" cols="20" rows="5" class="form-control"
+                                    autocomplete="off"></textarea>
                             </div>
                         </div>
                         <div class="row ">
@@ -37,10 +36,14 @@
                                 </select>
                             </div>
                         </div>
-
+                        <div class="form-group">
+                            <label for="file">Cargar archivo</label>
+                            <input type="file" name="file" id="file" class="form-control-file">
+                        </div>
                         <div class="text-center">
                             <div class="btn-group mt-3">
-                                <a href="{{route('questions_index')}}" class="btn btn-sm btn-outline-secondary">Regresar</a>
+                                <a href="{{route('questions_index')}}"
+                                    class="btn btn-sm btn-outline-secondary">Regresar</a>
                                 <button type="submit" class="btn btn-sm btn-outline-success">Guardar</button>
                             </div>
                         </div>
