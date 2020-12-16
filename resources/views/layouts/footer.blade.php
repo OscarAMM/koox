396fbbe53063f2d8c029d9c94ea34f9d0ba0a867
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-12-16 09:21:09 -0600

    User options added at footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -80,6 +80,34 @@
                             </a>
                         </li>
                         <li><a href="#" class="text-white"> <i class="fas fa-envelope"></i> Contacto </a></li>
+                        @guest
+                        <li class="list-inline-item">
+                            <p class="text-white"> <a href="{{ route('login') }}" class="text-white"><i
+                                        class="fas fa-sign-in-alt"></i>
+                                    {{ __('Iniciar sesión') }}</a></p>
+                        </li>
+                        @else
+                        <li class="list-inline-item dropdown">
+                            <a id="navbarDropdown" class="dropdown-toggle text-white" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <i class="fas fa-user"></i> {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    <i class="fas fa-sign-out-alt"></i> Cerrar sesión
+                                </a>
+                                @if(Auth::user()->hasRole('Administrador'))
+                                <a class="dropdown-item" href="{{route('home')}}">
+                                    <i class="fas fa-cog"></i> Administrar sitio
+                                </a>
+                                @endif
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
                     </ul>
                 </div>
                 <div class="col-md-2 text-left">
